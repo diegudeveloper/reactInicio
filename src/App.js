6e1b2303a5403fc1f154ff6e038de3e281a7ce35
Diegu <diegudeveloper@gmail.com>
2024-02-09 18:41:21 +0100

    configuracion de componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import Card from './components/Card'
-
+import Card from './components/Card';
+import img01 from "./img/img01.jpg";
+import img02 from "./img/img02.jpg";
+import img03 from "./img/img03.jpg";
 
 function App() {
   return (
     <div>
-      <h1>Hola Mundo desde ract</h1>
-      <Card />
+      <h1>Componentes con React</h1>
+      <div className="Cards-wrapper">
+        <Card 
+          imageSrc={img01}
+          title="Título de la segunda tarjeta"
+          description="Descripción de la segunda tarjeta."
+        />
+        <Card 
+          imageSrc={img02}
+          title="Título de la segunda tarjeta"
+          description="Descripción de la segunda tarjeta."
+        />
+        <Card 
+          imageSrc={img03}
+          title="Título de la tercera tarjeta"
+          description="Descripción de la tercera tarjeta."
+        />
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './Card.css';
-import img01 from '../img/img01.jpg';
 
-
-const Card = () => {
+const Card = ({ imageSrc, title, description }) => {
     return ( 
         <div className="Card-container">
-            <img src={img01}></img>
-            <h3>Agencia de Viaje - Maqueta</h3>
-            <p>
-            Proyecto web sobre Agencia donde podrás comprar boletos de avión, paquetes de estadias y reservas de hospedaje.
-            </p>
-            <div>
+            <img src={imageSrc} alt="Imagen de la tarjeta"></img>
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <div className="Containerbuttons">
                 <button>Ver Codigo</button>
                 <button>Ver Demo</button>
             </div>
